0d9f5abfe969b097e809d6c6d41f30d1aa58c550
JM3ad <jack-mead@hotmail.co.uk>
2021-06-10 18:30:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Socketry frontend
+Typescript/react project hosted through github pages
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Setup
+### First time
+`npm install`
+
+### Dev
+`npm start`
+
+### Test
+`npm test`
+
+### Environment variables
+If you want to override locally, copy the .env file and rename the copy to .env.local; set any variables there.
+Remember to prefix with `REACT_APP_`
+
+### Deploy
+* Automatic on push to main
+* Manually: `npm run predeploy && npm run deploy`
+* Page is currently accessible at: https://jm3ad.github.io/hosted-react-app/
+
+## Using this as a template
+Create a new repository using this one as the template
+By default, you should adjust:
+* In `package.json`, the name & homepage
+* In `404.html` adjust the redirect URL
+* In `.env`, the REACT_APP_BASE_URL variable
---
+++ src/app/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import PageOne from 'app/pages/page_one';
+import PageTwo from 'app/pages/page_two'
+
+const App: React.FC = () => {
+  const redirect = sessionStorage.redirect;
+  delete sessionStorage.redirect;
+  if (redirect && redirect != location.href) {
+    history.replaceState(null, process.env.REACT_APP_BASE_URL || '', redirect);
+  }
+
+  return (
+  <Router basename={process.env.REACT_APP_BASE_URL}>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/page_two">Alternative Page</Link>
+        </li>
+      </ul>
+
+      <hr />
+      <Switch>
+        <Route exact path="/">
+          <PageOne />
+        </Route>
+        <Route path="/page_two">
+          <PageTwo />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+  );
+}
+
+export default App;
